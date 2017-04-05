186dd7074986c9fa9fb6a18e08668cfce917767d
Mike <maholzbach@gmail.com>
2017-04-05 22:18:21 +0200

    Sync

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-$("#riverInfoButton").click(function() {
-  $(".thumbnail").hide("drop", {direction: "down"}, 500, function() {
-    $("#riverInfo").show("fade", 500);
+$(function() {
+    $("#datepicker").datepicker();
   });
-});
-
-$("#riverInfo").click(function() {
-  $(this).hide("fade", 500, function() {
-    $(".thumbnail").show("fade", 500);
-  });
-});
